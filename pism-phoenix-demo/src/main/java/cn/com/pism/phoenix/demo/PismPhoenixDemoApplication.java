c62987f99a9cc69cd5b68cc2991aeb42e0f89021
PerccyKing <perccyking@gmail.com>
2024-06-22 17:10:04 +0800

    feat PMNX-2 #正在进行 #comment 测试状态流转

--- pism-phoenix-demo/src/main/java/cn/com/pism/phoenix/demo/PismPhoenixDemoApplication.java
+++ pism-phoenix-demo/src/main/java/cn/com/pism/phoenix/demo/PismPhoenixDemoApplication.java
@@ -1,11 +1,13 @@
 package cn.com.pism.phoenix.demo;
 
 import cn.com.pism.phoenix.starter.annotations.EnablePismPhoenix;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @EnablePismPhoenix
+@Log4j2
 public class PismPhoenixDemoApplication {
 
     public static void main(String[] args) {
