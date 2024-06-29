9bd5d4057a9a0f33f4c7a06339dc4ca339866910
PerccyKing <perccyking@gmail.com>
2024-06-29 11:30:24 +0800

    feat #PMNX-1 #comment 补充一些基本信息

--- pism-phoenix-core/src/main/java/cn/com/pism/phoenix/core/config/PmnxKnife4jConfiguration.java
+++ pism-phoenix-core/src/main/java/cn/com/pism/phoenix/core/config/PmnxKnife4jConfiguration.java
@@ -1,7 +1,9 @@
 package cn.com.pism.phoenix.core.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -26,9 +28,22 @@ public class PmnxKnife4jConfiguration {
 
     @Bean
     public OpenAPI baseInfo() {
+        License license = new License();
+        license.setName("Apache 2.0");
+        license.setUrl("http://www.apache.org/licenses/LICENSE-2.0.html");
+
+        Contact contact = new Contact();
+        contact.setName("https://github.com/pism-oss");
+        contact.setUrl("https://github.com/pism-oss");
+        contact.setEmail("oss@pism.com.cn");
+
         return new OpenAPI()
                 .info(new Info()
                         .title("Pism Phoenix Core")
+                        .description("Pism Phoenix Core")
+                        .contact(contact)
+                        .summary("summary")
+                        .license(license)
                         .version("v0.0.1"));
     }
 }
